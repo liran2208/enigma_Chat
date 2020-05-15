dfbc01ee95b3ef4c13361dc559d9bfc5b2525017
liran2208 <65400001+liran2208@users.noreply.github.com>
2020-05-15 15:52:26 +0300

    first commit

---
+++ client.py
@@ -0,0 +1,68 @@
+import socket
+import _thread
+import threading
+import time
+import sys
+import enigma
+from os import system
+import datetime
+# -----daily enigma key & settings---------
+rotors = [5, 3, 1, 'b']
+states = [21, 13, 11]
+rotors = enigma.initialize_states(rotors, states)
+plugboard = ["ab", "xy", "hj", "po", "mn", "ld", "qw", "fr", "ev", "zs"]
+# -----------------------------------------
+def receive_messages(recieve_socket):
+    while True:
+        msg = recieve_socket.recv(1024).decode()
+        print (enigma.cypher(msg, rotors, plugboard))
+        # print (recieve_socket.recv(1024).decode())
+
+def send_messages(send_socket, userName):
+    while True:
+        msg = input()
+        if msg == "exit()":
+            print ("exiting code...")
+            # send_socket.close()
+            sys.exit()
+        send_socket.send((enigma.cypher(userName + ": " + msg, rotors, plugboard)).encode())
+        # send_socket.send(input().encode())
+
+def main():
+    system('cls')
+    """
+    if len(sys.argv) != 3:
+        print("Correct usage: script, IP address, port number")
+        exit()
+    """
+    my_socket = socket.socket()
+
+    again = True
+    while again:
+        try: 
+            IP_address = "127.0.0.1" # str(input("Enter the chat's IP: ")) # str(sys.argv[1])
+            Port = 1234 #int(input("Enter the chat's port number: ")) # int(sys.argv[2])
+            my_socket.connect((IP_address, Port))
+            again = False
+        except:
+            system('cls')
+            print ("IP/Port not found. please try again")
+            again = True
+        
+    # my_socket.connect((IP_address, Port))
+    userName = input("Connected. Welcome To This Chatroom. please enter username: ")
+    # print (my_socket)
+    t = threading.Thread(target=receive_messages, args=(my_socket, ))
+    j = threading.Thread(target=send_messages, args=(my_socket, userName))
+    t.start()
+    j.start()
+    t.join()
+    j.join()
+    """
+    time.sleep(1)  #this delay lets the threads kick in, otherwise the thread count is zero and it crashes
+    while _thread._count() > 1:
+        pass
+    """
+
+if __name__ == '__main__':
+    main()
---
+++ enigma.py
@@ -0,0 +1,171 @@
+from functools import reduce
+rotor_one = "ekmflgdqvzntowyhxuspaibrcj"
+rotor_two = "ajdksiruxblhwtmcqgznpyfvoe"
+rotor_three = "bdfhjlcprtxvznyeiwgakmusqo"
+rotor_four = "esovpzjayquirhxlnftgkdcmwb"
+rotor_five = "vzbrgityupsdnhlxawmjqofeck"
+ref_a = "ejmzalyxvbwfcrquontspikhgd"
+ref_b = "yruhqsldpxngokmiebfzcwvjat"
+ref_c = "fvpjiaoyedrzxwgctkuqsbnmhl"
+
+rotor_one_reset = "ekmflgdqvzntowyhxuspaibrcj"
+rotor_two_reset = "ajdksiruxblhwtmcqgznpyfvoe"
+rotor_three_reset = "bdfhjlcprtxvznyeiwgakmusqo"
+rotor_four_reset = "esovpzjayquirhxlnftgkdcmwb"
+rotor_five_reset = "vzbrgityupsdnhlxawmjqofeck"
+ref_a_reset = "ejmzalyxvbwfcrquontspikhgd"
+ref_b_reset = "yruhqsldpxngokmiebfzcwvjat"
+ref_c_reset = "fvpjiaoyedrzxwgctkuqsbnmhl"
+reset_list = []
+
+day_states = [1, 1, 1]
+
+def initialize_states(rotors, states):
+    """
+    :param rotors:
+    :param states:
+    :return: set the starting settings of the enigma. it sets daily
+    """
+    # day_states = states[:]
+
+    rotors_lst = []
+    for i in rotors:
+        if i == 1:
+            rotors_lst.append(rotor_one)
+        if i == 2:
+            rotors_lst.append(rotor_two)
+        if i == 3:
+            rotors_lst.append(rotor_three)
+        if i == 4:
+            rotors_lst.append(rotor_four)
+        if i == 5:
+            rotors_lst.append(rotor_five)
+        if i == 'a':
+            rotors_lst.append(ref_a)
+        if i == 'b':
+            rotors_lst.append(ref_b)
+        if i == 'c':
+            rotors_lst.append(ref_c)
+    for i in range(len(rotors)-1):
+        rotors_lst[i] = rotors_lst[i][-states[i]+1:] + rotors_lst[i][:-states[i]+1]
+    # print("initialize_states: ", rotors_lst)
+
+    return rotors_lst
+
+
+def switchLetters(pears, letter):
+    """
+    :param pears:
+    :param letter:
+    :return: the string after the action of the plugboard
+    """
+    for i in range(len(pears)):
+        if letter in pears[i]:
+            if letter == pears[i][1]:
+                return pears[i][0]
+            else:
+                return pears[i][1]
+    return letter
+
+
+def add_states(states, rotors):
+    """
+    :param states:
+    :param rotors:
+    :return: the changed and updated rotors
+    the rotor's way of operation is like a clock, that the first rotors is the seconds,
+    the second is the minutes and the third is the hours, when the second hand is above
+    a certain number,  it resets and the to the seconds hand is added one...
+    (same with hours and minutes)
+    """
+    states[0] += 1
+    rotors[0] = change_state(rotors[0], 1)
+    for i in range(len(states) - 1):
+        if states[i] > 26:
+            states[i] -= 26
+            states[i + 1] += 1
+            rotors[i+1] = change_state(rotors[i + 1], 1)
+
+
+def change_state(r, state):
+    """
+    :param r:
+    :param state:
+    :return: rotated string (r), (state) times
+    this functions is used to rotate the rotors of the enigma.
+    """
+    # changes the state of the rotor once(state++), rotates the string 'r' once
+    return r[-state:] + r[:-state]
+
+
+def rotor(rotors, msg, states):
+    """
+    :param rotors:
+    :param msg:
+    :param states:
+    :return: the string after the action of the rotors.
+    """
+    output = []
+    for i in range(len(msg)):
+        if msg[i].isalpha():
+            case = msg[i].isupper()
+            char = msg[i].lower()
+            char = reduce(lambda res, rot: rot[ord(res) - ord("a")], rotors, char.lower())
+            char = reduce(lambda res, rot: chr(rot.index(res) + ord("a")), rotors[2::-1], char)
+            output.append(char.upper()) if case else output.append(char)
+            add_states(states, rotors)
+        else:
+            output.append(msg[i])
+    # print "inside rotors2: ",  states
+    for i in range(len(states)):
+        # print "the states[i] to change back the settings: ", states[i]
+        rotors[i] = change_state(rotors[i], 0 - states[i]+1)
+    return output
+
+
+def addOne(msg):
+    lst = [char for char in msg]
+    new_lst = []
+    for i in lst:
+        new_lst.append(chr(ord(i) + msg.find(i)))
+    return ''.join(new_lst)
+
+
+def cypher(msg, rotors, plugboard):
+    """
+    :param msg:
+    :param rotors:
+    :param plugboard:
+    :return: enigma method encryption message
+    """
+    states = day_states[:]
+    # print "rotor_settings = ", rotor_settings
+    lst = [char for char in msg]
+
+    for i in range(len(lst)):
+        lst[i] = (switchLetters(plugboard, lst[i]))
+
+    lst = rotor(rotors, lst, states)
+
+    for i in range(len(lst)):
+        lst[i] = switchLetters(plugboard, lst[i])
+    encrypted = ''.join(lst)
+
+    return encrypted
+
+
+def main():
+
+    # ---------------insert here your settings------------------------------
+    rotors = [1, 4, 5, 'c']
+    rotor_settings = [1, 1, 1]
+    plugboard = ["ab", "xy", "hj", "po", "mn", "ld", "qw", "fr", "ev", "zs"]
+    rotors = initialize_states(rotors, rotor_settings)
+    msg = "xjmsr"
+    # ---------------------------------------------------------------------
+
+    print ("output: " + cypher(msg, rotors, plugboard))
+
+if __name__ == "__main__":
+    main()
+
